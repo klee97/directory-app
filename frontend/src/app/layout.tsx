3bd5f8b6c6efafaa70b74fd65120e8d79775f0ee
klee97 <klee9700@gmail.com>
2025-02-19 12:04:38 -0800

    Add microsoft clarity heatmap

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,6 +5,7 @@ import ThemeProvider from '@/components/theme/ThemeProvider';
 import Navbar from "@/components/layouts/Navbar";
 import FeedbackPopup from "@/features/contact/components/FeedbackPopup";
 import defaultImage from '@/assets/placeholder_cover_img_heart.jpeg';
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: 'Asian Wedding Hair & Makeup Directory – Find Trusted Artists',
@@ -40,7 +41,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID as string} />
+      {/* Microsoft Clarity */}
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "qcfdyqnpxk");`,
+        }}
+      />
       <body>
         <AppRouterCacheProvider>
           <ThemeProvider>
