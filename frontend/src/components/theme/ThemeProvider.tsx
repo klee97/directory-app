0f930a472f707e9fed3c378380f0411b1741a52c
klee97 <klee9700@gmail.com>
2025-02-20 10:09:16 -0800

    set default theme to be light

--- frontend/src/components/theme/ThemeProvider.tsx
+++ frontend/src/components/theme/ThemeProvider.tsx
@@ -8,7 +8,7 @@ export default function ThemeProvider({ children }: { children: ReactNode }) {
   const theme = createTheme(getDesignTokens());
 
   return (
-    <MuiThemeProvider theme={theme}>
+    <MuiThemeProvider theme={theme} defaultMode="light">
       <CssBaseline />
       {children}
     </MuiThemeProvider>
