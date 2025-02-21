a98182034fa1a9fd81d122b6c82cbf9cddcbb58a
klee97 <klee9700@gmail.com>
2025-02-21 13:46:52 -0800

    update about page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -21,12 +21,13 @@ export default function About() {
           About Us
         </Typography>
         <Typography variant="body1" component="p">
-          As two Asian American women, we understand how overwhelming wedding planning can be—especially when you&apos;re straddling the line between different cultures.
+          As two Asian American women, we understand how difficult it is to find wedding artists who know how to highlight your natural beauty.
+          A makeup artist who is an experienced professional is not necessarily experienced with Asian features or styles of makeup. 
         </Typography>
         <Typography variant="body1" component="p">
           We&apos;re building this directory to help Asian couples find artists who are recommended within their local communities.
-          We&apos;re asking our friends and family, scouring the web, and looking through Reddit and Facebook, so you don&apos;t have to.
-          The artists here are people who other couples have recommended, and we&apos;re excited to share their knowledge with you.
+          We&apos;re asking our friends and family, scouring the web, scrolling Instagram, and looking through Reddit and Facebook, so you don&apos;t have to.
+          The artists here are people who other couples have recommended, and we&apos;re excited to share their recommendations with you.
         </Typography>
         <Typography variant="body1" component="p">
           Whether you&apos;re a vendor looking to showcase your work or a couple planning your big day, we&apos;re here to help.
