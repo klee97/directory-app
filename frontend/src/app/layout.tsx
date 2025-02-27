ce72de99bd669580e1fce1c81bfc1de582f259c6
imalao <ivy@cookin.com>
2025-02-27 17:46:31 -0500

    Add preview image to all metadata and update image per PR comments

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,16 +6,34 @@ import Navbar from "@/components/layouts/Navbar";
 import FeedbackPopup from "@/features/contact/components/FeedbackPopup";
 import Script from "next/script";
 import { Footer } from "@/components/layouts/Footer";
+import previewImage from '@/assets/website_preview.jpeg';
 
 export const metadata: Metadata = {
   title: 'Asian Wedding Hair & Makeup – Find artists in NY, LA & more',
   description: 'Find hair and makeup artists recommended for Asian brides. Compare prices, search by location, and book the right artist for you.',
+  openGraph: {
+    title: 'Vendor Directory - Find the Best Wedding Artists',
+    description: 'Find hair and makeup artists recommended for Asian brides. Compare prices, search by location, and book the right artist for you.',
+    url: 'https://www.asianweddingmakeup.com/',
+    type: 'website',
+    images: [
+      {
+        url: previewImage.src,
+        width: 1200,
+        height: 630,
+        alt: 'Wedding Vendor Directory Preview',
+      },
+    ],
+  },
   twitter: {
     card: 'summary_large_image',
     title: 'Hair and Makeup Wedding Directory for Asian Beauty - Find Artists Experienced with Asian Features',
     description: 'Find hair and makeup artists recommended for Asian brides. Compare prices, search by location, and book the right artist for you.',
-    // images: ['https://yourwebsite.com/preview-image.jpg'],
-  }
+    images: [previewImage.src],
+  },
+  alternates: {
+    canonical: 'https://asianweddingmakeup.com/',
+  },
 };
 export default function RootLayout({
   children,
