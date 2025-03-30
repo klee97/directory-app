b0387533056f06d0a26f867bcce63d7162932d49
imalao <ivy@cookin.com>
2025-03-29 22:52:17 -0400

    upsert user_favorites row, initial code for favorites page

--- frontend/src/features/business/api/fetchCustomer.ts
+++ frontend/src/features/business/api/fetchCustomer.ts
@@ -3,17 +3,31 @@ import { transformBackendCustomerToFrontend } from '@/types/customer';
 
 export async function fetchCustomerById(id: string) {
   console.debug("Fetching customer with ID: %s", id);
-  const { data: customer, error } = await supabase
+  const { data: customer, error: customerError } = await supabase
     .from('profiles')
     .select(`
-      *, 
-      user_favorites (user_id, vendor_id, is_favorited)
-    `)
+    *
+  `)
     .eq('id', id)
     .single();
-  if (error) {
-    console.error('Error fetching customer: %s', error);
+
+  console.log(customer)
+  if (customerError) {
+    console.error('Error fetching customer: %s', customerError);
     return null;
   }
-  return transformBackendCustomerToFrontend(customer);
+
+  const { data: favorites, error: favoritesError } = await supabase
+    .from('user_favorites')
+    .select(`
+      *
+    `)
+    .eq('user_id', id);
+
+  console.log(favorites)
+  if (favoritesError) {
+    console.error('Error fetching favorites: %s', favoritesError);
+    return null;
+  }
+  return transformBackendCustomerToFrontend(customer, favorites ?? []);
 }
--- frontend/src/types/customer.ts
+++ frontend/src/types/customer.ts
@@ -1,10 +1,9 @@
 import type { Database } from "@/types/supabase";
 
 
-export type BackendCustomer = Database['public']['Tables']['profiles']['Row'] & {
-    favorite_vendors: BackendCustomerFavorites[]
-}
+export type BackendCustomer = Database['public']['Tables']['profiles']['Row']
 
+export type BackendCustomerFavoritesInsert = Database['public']['Tables']['user_favorites']['Insert'];
 export type BackendCustomerFavorites = Database['public']['Tables']['user_favorites']['Row'];
 
 export type CustomerFavorite = Pick<BackendCustomerFavorites,
@@ -18,11 +17,11 @@ export type Customer = Pick<BackendCustomer, 'id'
     'favorite_vendors': CustomerFavorite[]
 }
 
-export function transformBackendCustomerToFrontend(customer: BackendCustomer): Customer {
+export function transformBackendCustomerToFrontend(customer: BackendCustomer, favorites: BackendCustomerFavorites[]): Customer {
     return {
         id: customer.id,
         role: customer.role,
-        favorite_vendors: customer.favorite_vendors
+        favorite_vendors: favorites
             .map((favorite) => {
                 return {
                     vendor_id: favorite.vendor_id,
