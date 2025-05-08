afaa172478a07306737f1ca069a36c2e33d6515c
klee97 <klee9700@gmail.com>
2025-05-08 15:30:11 -0700

    Add bundle analyzer commented out

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+// config for analyzing bundle size. Wrap exports in withBundleAnalyzer() to debug
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
 
 export default nextConfig;
 
