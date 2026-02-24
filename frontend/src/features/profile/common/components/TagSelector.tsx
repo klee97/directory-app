ea305349a4c6f91faec91a3ab0a4f9b2c52078dc
klee97 <klee9700@gmail.com>
2026-02-23 18:07:17 -0800

    fix repeated tags and missing saving overlay

--- frontend/src/features/profile/common/components/TagSelector.tsx
+++ frontend/src/features/profile/common/components/TagSelector.tsx
@@ -4,40 +4,47 @@ import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import { VendorTag } from '@/types/vendor';
 
 interface TagSelectorProps {
-    value: VendorTag[] | null;
-    onChange: (value: VendorTag[] | null) => void;
-    options: VendorTag[];
+  value: VendorTag[] | null;
+  onChange: (value: VendorTag[] | null) => void;
+  options: VendorTag[];
 }
 
 const TagSelector: React.FC<TagSelectorProps> = ({ value, onChange, options }) => {
-    const filter = createFilterOptions<VendorTag>();
+  const filter = createFilterOptions<VendorTag>();
 
-    const handleChange = (
-        event: React.SyntheticEvent<Element, Event>,
-        newValue: VendorTag[] | null
-    ) => {
-        // If user clears all tags, pass null instead of empty array
-        onChange(newValue && newValue.length > 0 ? newValue : null);
-    };
+  const handleChange = (
+    event: React.SyntheticEvent<Element, Event>,
+    newValue: VendorTag[] | null
+  ) => {
+    // If user clears all tags, pass null instead of empty array
+    onChange(newValue && newValue.length > 0 ? newValue : null);
+  };
 
-    return (
-        <Autocomplete<VendorTag, true, false, false>
-            value={value ?? []}
-            onChange={handleChange}
-            filterOptions={(opts, params) => filter(opts, params)}
-            multiple
-            disableCloseOnSelect
-            selectOnFocus
-            clearOnBlur
-            handleHomeEndKeys
-            options={options}
-            getOptionLabel={(option) => option.display_name ?? option.name ?? ''}
-            renderOption={(props, option) => <li {...props} key={option.id}>{option.display_name ?? option.name}</li>}
-            renderInput={(params) => (
-                <TextField {...params} fullWidth />
-            )}
-        />
-    );
+  return (
+    <Autocomplete<VendorTag, true, false, false>
+      value={value ?? []}
+      onChange={handleChange}
+      filterOptions={(opts, params) => {
+        const filtered = filter(opts, params);
+        // Exclude already-selected options
+        return filtered.filter(
+          (opt) => !(value ?? []).some((v) => v.id === opt.id)
+        );
+      }}
+      isOptionEqualToValue={(option, val) => option.id === val.id}
+      multiple
+      disableCloseOnSelect
+      selectOnFocus
+      clearOnBlur
+      handleHomeEndKeys
+      options={options}
+      getOptionLabel={(option) => option.display_name ?? option.name ?? ''}
+      renderOption={(props, option) => <li {...props} key={option.id}>{option.display_name ?? option.name}</li>}
+      renderInput={(params) => (
+        <TextField {...params} fullWidth />
+      )}
+    />
+  );
 };
 
 export default TagSelector;
