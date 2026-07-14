abf16ba4a47564a29a2d401fc618bc14ac8b6c36
imalao <imalao@uchicago.edu>
2026-07-14 17:42:21 -0400

    Address AI review comment

--- frontend/src/app/(public)/directory/page.tsx
+++ frontend/src/app/(public)/directory/page.tsx
@@ -73,7 +73,7 @@ export default async function DirectoryPage() {
       <section>
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd).replace(/</g, '\\u003c') }}
         />
       </section>
       <Directory vendors={shuffledVendors} tags={uniqueTags} />
