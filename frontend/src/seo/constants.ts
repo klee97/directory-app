a2049778821ad6163bc1a37cf95dff8d29a54401
klee97 <klee9700@gmail.com>
2026-08-10 16:00:38 -0700

    fix image url

--- frontend/src/seo/constants.ts
+++ frontend/src/seo/constants.ts
@@ -2,6 +2,6 @@ export const SITE_URL = "https://www.asianweddingmakeup.com";
 export const ORG_ID =  `${SITE_URL}#organization`;
 export const WEBSITE_ID = `${SITE_URL}#website`;
 
-export const LOGO_URL = `${SITE_URL}/images/logo.jpeg`;
-export const PHOTO_WEBSITE_PREVIEW_URL = `${SITE_URL}/images/photo_website_preview.jpg`;
-export const WEBSITE_PREVIEW_URL = `${SITE_URL}/images/website_preview.jpeg`;
+export const LOGO_URL = `${SITE_URL}/logo.jpeg`;
+export const PHOTO_WEBSITE_PREVIEW_URL = `${SITE_URL}/photo_website_preview.jpg`;
+export const WEBSITE_PREVIEW_URL = `${SITE_URL}/website_preview.jpeg`;
